b192385d92731e1f5c42e0ee14e9599c9444e062
bharathkm6767 <bharathkumarmb34342828@gmail.com>
2019-02-27 02:07:40 -0800

    programming

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask,render_template
+app=Flask(__name__)
+@app.route("/")
+def index():
+    return render_template("index.html")
+@app.route("/home")
+def home():
+    return render_template("index2.html")
+@app.route("/contact")
+def contact():
+     return render_template("index3.html")
+@app.route("/about")
+def about():
+    return render_template("index4.html")     
+
+
+if(__name__=="__main__"):
+    app.run(debug=True)
